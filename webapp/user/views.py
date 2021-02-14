2cf58d9d4735444a34d00cc3e802c80eb712c47d
Alexander P <virs3014@mail.ru>
2021-02-14 22:36:32 +0300

    Flask-Migrations и регистрация пользователей

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 
-from webapp.user.forms import LoginForm
+from webapp.db import db
+from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
@@ -35,3 +36,26 @@ def logout():
     logout_user()
     flash('Вы успешно разлогинились')
     return redirect(url_for('news.index'))
+
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    title = 'Регистрация'
+    form = RegistrationForm()
+    return render_template('user/registration.html', title=title, form=form)
+
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, role='user')
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!')
+        return redirect(url_for('user.login'))
+    flash('Пожалуйста, исправьте ошибки в форме')
+    return redirect(url_for('user.register'))
